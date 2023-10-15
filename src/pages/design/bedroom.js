4b13bb4f08eff55263154e85aba1a5df85c318fc
Gergo Buglyo <buglyogergo@gmail.com>
2023-10-15 23:14:49 +0200

    Created all room path with queries

---
+++ src/pages/design/bedroom.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import { graphql, useStaticQuery } from "gatsby";
+import Image from "../../components/Image";
+
+const Bedroom = () => {
+  const data = useStaticQuery(graphql`
+    query Images {
+      allFile(
+        filter: {
+          extension: { regex: "/(jpg)|(png)|(jpeg)/" }
+          relativeDirectory: { eq: "bedroom" }
+        }
+      ) {
+        edges {
+          node {
+            id
+            relativeDirectory
+            base
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const images = data.allFile.edges;
+
+  return (
+    <Layout>
+      {images.map((image) => {
+        // pass to the Image component image object from query
+        return <Image picture={image.node} key={image.node.id} />;
+      })}
+    </Layout>
+  );
+};
+
+export default Bedroom;
